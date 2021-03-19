04f38aa58b532c224a439b56ce2b39f333140928
zhangdaiscott <zhangdaiscott@163.com>
2021-03-19 10:49:28 +0800

    v1.3.0-beta版本发布，不再要求持久层必须mybatis-plus

--- jimureport-samples/jimureport-demo/src/main/java/com/example/jmreport/JimuReportApplication.java
+++ jimureport-samples/jimureport-demo/src/main/java/com/example/jmreport/JimuReportApplication.java
@@ -1,18 +1,19 @@
 package com.example.jmreport;
 
 import org.jeecg.modules.jmreport.common.util.oConvertUtils;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
-@MapperScan(value={"org.jeecg.modules.**.mapper*"})
+/**
+ * 积木报表独立服务启动类
+ */
 @SpringBootApplication(scanBasePackages = {"org.jeecg.modules.jmreport"})
-public class JimuReportDemoApplication {
+public class JimuReportApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext application = SpringApplication.run(JimuReportDemoApplication.class, args);
+        ConfigurableApplicationContext application = SpringApplication.run(JimuReportApplication.class, args);
         Environment env = application.getEnvironment();
         String port = env.getProperty("server.port");
         String path = oConvertUtils.getString(env.getProperty("server.servlet.context-path"));
