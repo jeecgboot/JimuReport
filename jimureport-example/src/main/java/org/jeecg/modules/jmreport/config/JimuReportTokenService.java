737f53f5228b31f3eae7c1bdd6541bf3be572dab
zhangdaiscott <zhangdaiscott@163.com>
2021-08-14 17:04:10 +0800

    1.3.78版本发布

--- jimureport-example/src/main/java/org/jeecg/modules/jmreport/config/JimuReportTokenService.java
+++ jimureport-example/src/main/java/org/jeecg/modules/jmreport/config/JimuReportTokenService.java
@@ -6,21 +6,27 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 自定义积木报表鉴权(如果不进行自定义，则所有请求不做权限控制)
+ * 1.自定义获取登录token
+ * 2.自定义获取登录用户
+ */
 @Component
 public class JimuReportTokenService implements JmReportTokenServiceI {
 
     @Override
     public String getToken(HttpServletRequest request) {
-        return "123";
+        return "123456";
     }
 
     @Override
-    public String getUsername(String s) {
+    public String getUsername(String token) {
         return "admin";
     }
 
     @Override
-    public Boolean verifyToken(String s) {
+    public Boolean verifyToken(String token) {
+        //token校验，默认返回true，需自己实现
         return true;
     }
 
