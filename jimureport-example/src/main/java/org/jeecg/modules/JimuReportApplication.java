eaad56d0a8472749afba8277f2c5ef0e4e161c06
zhangdaiscott <zhangdaiscott@163.com>
2022-03-05 14:33:02 +0800

    JimuReport积木报表 v1.4.3版本发布，免费的可视化低代码报表

--- jimureport-example/src/main/java/org/jeecg/modules/JimuReportApplication.java
+++ jimureport-example/src/main/java/org/jeecg/modules/JimuReportApplication.java
@@ -1,6 +1,5 @@
 package org.jeecg.modules;
 
-import org.jeecg.modules.jmreport.common.util.oConvertUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,7 +18,7 @@ public class JimuReportApplication {
         ConfigurableApplicationContext application = SpringApplication.run(JimuReportApplication.class, args);
         Environment env = application.getEnvironment();
         String port = env.getProperty("server.port");
-        String path = oConvertUtils.getString(env.getProperty("server.servlet.context-path"));
+        String path = env.containsProperty("server.servlet.context-path")?env.getProperty("server.servlet.context-path"):"";
         System.out.print("\n----------------------------------------------------------\n\t" +
                 "Application JimuReport Demo is running! Access URL:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/jmreport/list\n\t" +
