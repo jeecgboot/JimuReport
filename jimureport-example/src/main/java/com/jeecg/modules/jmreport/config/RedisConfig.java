eb7917d0fd48c1d368e0d0b17422849a96b32279
JEECG <445654970@qq.com>
2024-11-21 10:04:57 +0800

    1

--- jimureport-example/src/main/java/com/jeecg/modules/jmreport/config/RedisConfig.java
+++ jimureport-example/src/main/java/com/jeecg/modules/jmreport/config/RedisConfig.java
@@ -1,11 +1,18 @@
 package com.jeecg.modules.jmreport.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+/**
+ * RedisTemplate实例化
+ */
+@Slf4j
 @Configuration
+@ConditionalOnMissingBean(RedisTemplate.class)
 public class RedisConfig {
 
     @Bean
@@ -15,6 +22,7 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
+        log.info("---- jimubi Redis Template init----");
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
